f612a2a8d609f217170a2644811c475796a09c12
Narayan Hamde <narayan.hamde@cctech.co.in>
2025-07-13 14:44:13 +0530

    modified default template_1v

--- app/page.tsx
+++ app/page.tsx
@@ -5,48 +5,28 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+// import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
+// Amplify.configure(outputs);
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [inputWord, setInputWord] = useState("");
+  const [wordDesc, setWordDesc] = useState("");
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
+  const searchWord = () => {
+    setWordDesc("Searching...");
+    setTimeout(() => {
+      setWordDesc(`Definition of "${inputWord}" will appear here.`);
+    }, 1000);
+};
 
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
+      <h1>Cloud Dictionary</h1>
+      <button onClick={searchWord}>Search</button>
+      <p>{wordDesc}</p>
     </main>
   );
 }
